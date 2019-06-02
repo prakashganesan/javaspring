716f7093a84a4ff18aad60c5562c6882128653dc
EC2 Default User <ec2-user@ip-172-31-7-146.us-east-2.compute.internal>
2019-06-02 16:38:43 +0000

    prod file

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World 2";
+        return "Hello Docker World prod";
     }
 
     public static void main(String[] args) {
